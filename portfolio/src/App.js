57ac4b8a6538e5219cdb1a9e0a707e2d5379abf9
bobbiro2e <bobbiro2e@gmail.com>
2018-10-04 15:35:32 -0500

    folder structure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/pages/Home/Home";
+import Bio from "./components/pages/Bio/Bio";
+import Work from "./components/pages/Work/Work";
+import Four from "./components/pages/FourOhFour/FourOhFour";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/bio" component={Bio} />
+            <Route exact path="/work" component={Work} />
+            <Route component={Four} />
+          </Switch>
+        </Router>
       </div>
     );
   }
