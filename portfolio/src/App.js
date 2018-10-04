5e6da6ad40c01bf15542abb881e3e66b3cb069a6
bobbiro2e <bobbiro2e@gmail.com>
2018-10-04 16:20:25 -0500

    favicon files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import Bio from "./components/pages/Bio/Bio";
 import Work from "./components/pages/Work/Work";
 import Four from "./components/pages/FourOhFour/FourOhFour";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 
